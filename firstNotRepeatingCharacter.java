6ea34658928850ee32bc5bf6c045125e7e87cbbf
swethiurs5 <swethiurs5@gmail.com>
2017-11-01 23:25:39 -0700

    Add files via upload
    Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
    Example
        For s = "abacabad", the output should be
        firstNotRepeatingCharacter(s) = 'c'.
    
        There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
    
        For s = "abacabaabacaba", the output should be
        firstNotRepeatingCharacter(s) = '_'.
    
        There are no characters in this string that do not repeat.

---
+++ firstNotRepeatingCharacter.java
@@ -0,0 +1,11 @@
+char firstNotRepeatingCharacter(String s) {
+        int freq [] = new int[26];
+        for(int i = 0; i < s.length(); i ++)
+            freq [s.charAt(i)-'a'] ++;
+        for(int i = 0; i < s.length(); i ++)
+            if(freq [s.charAt(i)-'a'] == 1)
+                return  s.charAt(i);
+        
+            return '_';
+   
+}
